a18c9fe59f9e85d47c8573217fed8a83f17ce9c5
Rozeri <rozeri.dilar@gmail.com>
2019-06-05 13:32:18 +0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Rozeri Dağtekin on 6/5/19.
+//  Copyright © 2019 Rozeri Dağtekin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
